c8a840b742d4b4536a657a25e79e20f33eab67dd
Christopher Cugliotta <christopher.cugliotta@altegrahealth.com>
2021-12-12 11:47:45 -0500

    Rename loaders to extractors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
-from cc_idea.loaders.reddit import get_comments
+from cc_idea.core.config import paths
+from cc_idea.extractors.reddit import get_comments
 from cc_idea.utils.log_utils import initialize_logger
 log = logging.getLogger('cc_idea')
 
@@ -8,13 +9,13 @@ log = logging.getLogger('cc_idea')
 
 if __name__ == '__main__':
 
-    initialize_logger()
+    initialize_logger(paths.repo / 'log.log')
     log.info('Begin.')
 
     df = get_comments(
         q='cardano',
         start_date=datetime(2021, 1, 1, 0, 0, 0),
-        end_date=datetime(2021, 1, 1, 5, 0, 0),
+        end_date=datetime(2021, 1, 2, 0, 0, 0),
     )
 
     log.info('Done.')
--- tests/extractors/test_reddit.py
+++ tests/extractors/test_reddit.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from cc_idea.loaders.reddit import _load_comments
+from cc_idea.extractors.reddit import _load_comments
 
 
 
