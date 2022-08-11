ce39b46d89c5f09d51adcd8f99f85aa0967df08c
Christopher Cugliotta <christopher.cugliotta@altegrahealth.com>
2022-08-11 11:46:20 -0400

    Fix broken test

--- tests/extractors/test_yahoo.py
+++ tests/extractors/test_yahoo.py
@@ -11,6 +11,6 @@ def test_load_prices():
     df = load_prices('MSFT')
     df = df.set_index('date')
 
-    assert round(df.loc['2018-12-31']['open'], 2) ==  97.95
-    assert round(df.loc['2019-12-31']['open'], 2) == 153.84
-    assert round(df.loc['2020-12-31']['open'], 2) == 219.86
+    assert round(df.loc['2018-12-31']['open'], 2) ==  97.52
+    assert round(df.loc['2019-12-31']['open'], 2) == 153.16
+    assert round(df.loc['2020-12-31']['open'], 2) == 218.89
